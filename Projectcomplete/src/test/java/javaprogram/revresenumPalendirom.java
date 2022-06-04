33b7e7b0bf4108631969595113524b554d61aff5
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-06-04 17:05:14 +0530

    #reversenumber

--- Projectcomplete/src/test/java/javaprogram/revresenumPalendirom.java
+++ Projectcomplete/src/test/java/javaprogram/revresenumPalendirom.java
@@ -1,5 +1,40 @@
 package javaprogram;
 
+import org.testng.annotations.Test;
+
 public class revresenumPalendirom {
+	
+	@Test
+	public void revrsenum() {
+		
+		int num=1234567891;
+		int revnum=0;
+		while(num>0) {
+		
+		int remind=num%10;
+		revnum=revnum*10+remind;
+		num=num/10;
+		}
+		System.out.println(revnum);
+	}
+	
+	
+	@Test
+	
+	public void palindrom() {
+		int num=78087;
+		int revnum=0;
+		int temp;
+		temp=num;
+		while(num>0) {
+			int remind=num%10;
+			revnum=revnum*10+remind;
+			num=num/10;
+			}if(temp==revnum) {
+				System.out.println("palindrom-->"+revnum);
+			}else {
+				System.out.println("not palindrom-->"+revnum);
+			}
+	}
 
 }
